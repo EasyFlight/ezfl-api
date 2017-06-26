8c62699fb9e06acb87e72d0424ce362d03b43bdf
Victor Ikoro <ikoro.victor@gmail.com>
2017-06-26 12:39:16 +0100

    static resources

---
+++ src/main/java/com/easyflight/flight/web/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.easyflight.flight.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Victor Ikoro on 6/26/2017.
+ */
+
+@Controller
+public class IndexController {
+}
