67d3718eebbbe331a004add6774689b6e9f55d3d
Victor Ikoro <ikoro.victor@gmail.com>
2017-09-16 11:29:54 +0100

    upport for mulitple client ids

--- src/main/java/com/easyflight/flight/oauth2/UserInfoPrincipal.java
+++ src/main/java/com/easyflight/flight/oauth2/UserInfoPrincipal.java
@@ -15,15 +15,19 @@ public class UserInfoPrincipal {
     private String firstName;
     private String lastName;
     private String email;
+    private String profile;
+    private String picture;
     private boolean emailVerified;
     private String infoProvider;
 
-    private  UserInfoPrincipal(Object id, Object name, Object firstName, Object lastName, Object email, boolean emailVerified, String infoProvider){
+    private  UserInfoPrincipal(Object id, Object name, Object firstName, Object lastName, Object email, Object profile, Object picture, boolean emailVerified, String infoProvider){
         this.id = (String) id;
         this.name = (String) name;
         this.firstName = (String) firstName;
         this.lastName = (String) lastName;
         this.email = (String) email;
+        this.profile = (String) profile;
+        this.picture = (String) picture;
         this.emailVerified = emailVerified;
         this.infoProvider = infoProvider;
     }
@@ -36,7 +40,8 @@ public class UserInfoPrincipal {
                 userInfo.get("given_name"),
                 userInfo.get("family_name"),
                 userInfo.get("email"),
-                Boolean.valueOf(userInfo.get("email_verified").toString()), InfoProvider.GOOGLE.name());
+                userInfo.get("profile"),
+                userInfo.get("picture"),Boolean.valueOf(userInfo.get("email_verified").toString()), InfoProvider.GOOGLE.name());
     }
 
     public String getId() {
@@ -71,4 +76,11 @@ public class UserInfoPrincipal {
         return infoProvider;
     }
 
+    public String getProfile() {
+        return profile;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
 }
